66e2b8e0c478bad02811d990afcf2dd1b9c5aa91
AbdulRhman <bodahassan12@gmail.com>
2024-04-13 15:11:52 +0200

    Fix drawing box size and make the grid more adjustable

--- javascript.js
+++ javascript.js
@@ -1,19 +1,25 @@
 const container = document.querySelector("#container");
 let isDrawing = false;
+let gridSize = 16;
 
+// To make drawing on grid possible in DOM
 function draw(event) {
     if(event.target.classList.contains("grid-item") && isDrawing) {
         const hoverdItem = event.target;
-        hoverdItem.style.backgroundColor = "red";
-        event.preventDefault();
+        hoverdItem.style.backgroundColor = "black";
+        event.preventDefault(); // Prevent mouse from breaking mouseup event
     }
 }
 
-// Creating grid elements
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+// Creating grid elements for rows
+for (let i = 0; i < gridSize; i++) {
+    // Another loop for columns within each row
+    for (let j = 0; j < gridSize; j++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
+        // Dividing each grid by the grid size
+        gridItem.style.width = `calc(100% / ${gridSize})`;
+        // Drawing effect
         gridItem.addEventListener("mousedown", (e) => {
             isDrawing = true;
             draw(e);
